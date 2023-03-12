262c64e0d4a73f7999be123e3db4ff08a8ab2403
MarianRapahel <73583313+MarianRaphael@users.noreply.github.com>
2023-03-12 10:43:09 +0100

    start at /device-agent

--- configs/additional_modules/device-agent-ui/app.js
+++ configs/additional_modules/device-agent-ui/app.js
@@ -8,7 +8,7 @@ const { spawn } = require('child_process');
 const configYml = './config.yml';
 app.use(express.static(path.join('./')));
 // Serve the HTML file when the user navigates to the root URL
-app.get('/', function (req, res) {
+app.get('/device-agent', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 // Handle form submission to save the configuration
