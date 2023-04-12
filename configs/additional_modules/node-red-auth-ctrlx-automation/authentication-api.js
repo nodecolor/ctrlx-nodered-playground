2e8aa10153b2088f80d256fe717a22d80874d51a
MarianRapahel <73583313+MarianRaphael@users.noreply.github.com>
2023-04-12 12:57:19 +0200

    update

--- configs/additional_modules/node-red-auth-ctrlx-automation/authentication-api.js
+++ configs/additional_modules/node-red-auth-ctrlx-automation/authentication-api.js
@@ -1,64 +1,80 @@
 const https = require('https');
 const jwtdecode = require('jwt-decode');
 
-const checkUserAccess = (jwt, access) => {
-  jwt.scope.forEach((element) => {
-    if (access === '' && element === 'ctrlx-node-red-flowforge.web.r') {
-      access = 'read';
-    } else if (
-      access === '' &&
-      element === 'ctrlx-node-red-flowforge.web.rwx'
-    ) {
-      access = '*';
-    } else if (access === '' && element === 'rexroth-device.all.rwx') {
-      access = '*';
-    } else if (
-      access === 'read' &&
-      element === 'ctrlx-node-red-flowforge.web.rwx'
-    ) {
-      access = '*';
-    } else if (access === '*' && element === 'ctrlx-node-red-flowforge.web.r') {
-      access = '*';
-    } else if (access === 'read' && element === 'rexroth-device.all.rwx') {
-      access = '*';
-    } else if (access === '*' && element === 'rexroth-device.all.rwx') {
-      access = '*';
-    }
-  });
-  return access;
-};
-
 module.exports = {
-  // checkPermissions checks the Node-RED permissions and returns a valid user or null if access denied
-  checkPermissions: function (username, password, callback) {
-    module.exports.token(username, password, (token) => {
-      if (token !== null) {
-        let jwt;
-        try {
-          jwt = jwtdecode(token);
-        } catch (e) {
-          callback(null);
-          return;
-        }
-        const access = checkUserAccess(jwt, '');
+  //checkPermissions checks the Node-RED permissions and returns a valid user or null if access denied
+  checkPermissions: function (token, callback) {
+    //console.log('checkPermissions:', token);
 
-        if (access !== '') {
-          callback({
-            username: jwt.name,
-            permissions: access,
-          });
-        } else {
-          callback(null);
-        }
-      } else {
-        callback(null);
+    var jwt;
+    try {
+      jwt = jwtdecode(token);
+    } catch (e) {
+      callback(null);
+      return;
+    }
+    user = null;
+    access = '';
+
+    jwt.scope.forEach((element) => {
+      if (access == '' && element == 'ctrlx-node-red-flowforge.web.r') {
+        access = 'read';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (
+        access == '' &&
+        element == 'ctrlx-node-red-flowforge.web.rwx'
+      ) {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (access == '' && element == 'rexroth-device.all.rwx') {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (
+        access == 'read' &&
+        element == 'ctrlx-node-red-flowforge.web.rwx'
+      ) {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (access == '*' && element == 'ctrlx-node-red-flowforge.web.r') {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (access == 'read' && element == 'rexroth-device.all.rwx') {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
+      } else if (access == '*' && element == 'rexroth-device.all.rwx') {
+        access = '*';
+        user = {
+          username: jwt.name,
+          permissions: access,
+        };
       }
     });
+    callback(user);
   },
 
-  // validate validates a given token and returns true if token valid, else false
-  validate: function (username, password, callback) {
-    const options = {
+  //validate validates a given token and returns true if token valid, else false
+  validate: function (token, callback) {
+    //console.log('validate:', token);
+
+    var options = {
       hostname: 'localhost',
       path: '/identity-manager/api/v1/auth/token/validity',
       method: 'GET',
@@ -68,7 +84,8 @@ module.exports = {
         authorization: 'Bearer ' + token,
       },
     };
-    const req = https.request(options, (res) => {
+
+    var req = https.request(options, (res) => {
       let chunks = [];
 
       res
@@ -79,8 +96,10 @@ module.exports = {
           let data = Buffer.concat(chunks);
           let jsonObject = JSON.parse(data);
 
+          //console.log('validation result:', JSON.stringify(jsonObject));
+
           if (jsonObject.valid === true) {
-            module.exports.checkPermissions(username, password, callback);
+            module.exports.checkPermissions(token, callback);
             return;
           }
           callback(null);
@@ -95,12 +114,16 @@ module.exports = {
     req.end();
   },
 
-  token: function (username, password, callback) {
-    const data = JSON.stringify({
+  //authenticate checks the username and password and returns a new valid token or null for unknown users
+  authenticate: function (username, password, callback) {
+    //console.log('authenticate:', username);
+
+    var data = JSON.stringify({
       name: username,
       password: password,
     });
-    const options = {
+
+    var options = {
       hostname: 'localhost',
       path: '/identity-manager/api/v2/auth/token',
       method: 'POST',
@@ -110,7 +133,7 @@ module.exports = {
       },
     };
 
-    const req = https.request(options, (res) => {
+    var req = https.request(options, (res) => {
       let chunks = [];
       res
         .on('data', (d) => {
@@ -119,7 +142,16 @@ module.exports = {
         .on('end', function () {
           let data = Buffer.concat(chunks);
           let jsonObject = JSON.parse(data);
-          callback(jsonObject.access_token);
+          module.exports.license(jsonObject.access_token, (valid) => {
+            if (valid === true) {
+              module.exports.checkPermissions(
+                jsonObject.access_token,
+                callback
+              );
+            } else {
+              callback(null);
+            }
+          });
         });
     });
 
@@ -132,29 +164,14 @@ module.exports = {
     req.end();
   },
 
-  authenticate: function (username, password, callback) {
-    module.exports.token(username, password, (token) => {
-      if (token !== null) {
-        module.exports.license(token, (valid) => {
-          if (valid === true) {
-            module.exports.checkPermissions(username, password, callback);
-          } else {
-            callback(null);
-          }
-        });
-      } else {
-        callback(null);
-      }
-    });
-  },
-
   license: function (token, callback) {
     function checkLicense(licenseName, token, successCallback, errorCallback) {
-      const payload = JSON.stringify({
+      var payload = JSON.stringify({
         name: licenseName,
         version: '1.0',
       });
-      const options = {
+  
+      var options = {
         hostname: 'localhost',
         path: '/license-manager/api/v1/license',
         method: 'POST',
@@ -164,39 +181,40 @@ module.exports = {
           Authorization: 'Bearer ' + token,
         },
       };
-
-      const req = https.request(options, (res) => {
+  
+      var req = https.request(options, (res) => {
         if (res.statusCode === 200) {
           successCallback();
         } else {
           errorCallback();
         }
       });
-
+  
       req.on('error', (error) => {
         errorCallback();
       });
-
+  
       req.write(payload);
       req.end();
     }
-
-    const license1 = 'SWL-W-XCx-NREDxFLOWxxxxxx-Y1NN';
-    const license2 = 'SWL_XCR_ENGINEERING_4H';
-
-    let checkCounter = 0;
+  
+    var license1 = 'SWL-W-XCx-NREDxFLOWxxxxxx-Y1NN';
+    var license2 = 'SWL_XCR_ENGINEERING_4H';
+  
+    var checkCounter = 0;
     function onSuccess() {
       callback(true);
     }
-
+  
     function onError() {
       checkCounter++;
       if (checkCounter === 2) {
+        log.warning("No valid Node-RED license is installed or the current license has expired.");
         callback(false);
       }
     }
-
+  
     checkLicense(license1, token, onSuccess, onError);
     checkLicense(license2, token, onSuccess, onError);
-  },
+  },  
 };
--- configs/additional_modules/node-red-auth-ctrlx-automation/user-authentication.js
+++ configs/additional_modules/node-red-auth-ctrlx-automation/user-authentication.js
@@ -9,30 +9,48 @@ module.exports = {
   //sessionExpiryTime: 36000, // 10h
   //tokenHeader: 'Bearer',
 
-  users: function (username, password) {
+  users: function (username) {
     return new Promise(function (resolve) {
-      api.checkPermissions(username, password, (user) => {
+      // Do whatever work is needed to check username is a valid
+      // user.
+      valid = true;
+      if (valid) {
+        // Resolve with the user object. It must contain
+        // properties 'username' and 'permissions'
+        var user = {
+          username: username,
+          permissions: '*'
+        };
         resolve(user);
-      });
+      } else {
+        // Resolve with null to indicate this user does not exist
+        resolve(null);
+      }
     });
   },
-  tokens: function (username, password) {
+  tokens: function (token) {
     return new Promise(function (resolve, reject) {
-      api.validate(username, password, (user) => {
+      // Do whatever work is needed to check token is valid
+      api.validate(token, (user) => {
         resolve(user);
-      });
+      })
     });
   },
   authenticate: function (username, password) {
     return new Promise(function (resolve) {
+      // Do whatever work is needed to validate the username/password
+      // combination.
       api.authenticate(username, password, (user) => {
         resolve(user);
-      });
+      })
     });
   },
   default: function () {
     return new Promise(function (resolve) {
+      // Resolve with the user object for the default user.
+      // If no default user exists, resolve with null.
+      // resolve({anonymous: true, permissions:'read'});
       resolve(null);
     });
-  },
-};
+  }
+}
